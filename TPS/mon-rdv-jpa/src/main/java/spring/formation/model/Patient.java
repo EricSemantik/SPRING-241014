bfcbe5630ee89875e7a4cd01e12d2a776fedd49a
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2024-10-15 10:31:50 +0200

    Atelier : Exercice 1 Starter

---
+++ TPS/mon-rdv-jpa/src/main/java/spring/formation/model/Patient.java
@@ -0,0 +1,49 @@
+package spring.formation.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patient extends Individu {
+	private LocalDate dtNaissance;
+	private String lieuNaissance;
+	private String numeroSS;
+	private List<Consultation> consultations = new ArrayList<Consultation>();
+
+	public Patient() {
+		super();
+	}
+
+	public LocalDate getDtNaissance() {
+		return dtNaissance;
+	}
+
+	public void setDtNaissance(LocalDate dtNaissance) {
+		this.dtNaissance = dtNaissance;
+	}
+
+	public String getLieuNaissance() {
+		return lieuNaissance;
+	}
+
+	public void setLieuNaissance(String lieuNaissance) {
+		this.lieuNaissance = lieuNaissance;
+	}
+
+	public String getNumeroSS() {
+		return numeroSS;
+	}
+
+	public void setNumeroSS(String numeroSS) {
+		this.numeroSS = numeroSS;
+	}
+
+	public List<Consultation> getConsultations() {
+		return consultations;
+	}
+
+	public void setConsultations(List<Consultation> consultations) {
+		this.consultations = consultations;
+	}
+
+}
