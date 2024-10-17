eb3abde52e71aac3f991ac2e2bb633c0bc652830
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2024-10-17 14:33:38 +0200

    Atelier 4 : Exercice 2 Erratum

---
+++ TPS/mon-rdv/src/main/java/spring/formation/web/HomeController.java
@@ -0,0 +1,35 @@
+package spring.formation.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String defaut() {
+		return "forward:/accueil";
+	}
+
+	@GetMapping("/accueil")
+	public String welcome(@RequestParam(name = "nom", required = false) String username, Model model) {
+		if(username != null && !username.isEmpty()) {
+			model.addAttribute("monNom", username);
+		}
+		
+		return "home";
+	}
+	
+	@GetMapping({"/home", "/home/{nom}"})
+	public String home(@PathVariable(name="nom", required = false) String username, Model model) {
+		if(username != null && !username.isEmpty()) {
+			model.addAttribute("monNom", username);
+		}
+		
+		return "home";
+	}
+	
+}
